6be5851ea816a561d3f90522286b891723f1940c
mohamed <mohamed.bd.business@gmail.com>
2022-12-25 23:44:23 +0100

    edite view

--- contactUs/views.py
+++ contactUs/views.py
@@ -3,12 +3,16 @@ from django.contrib.auth.decorators import login_required
 from account.decorators import *
 from .models import *
 from .forms import *
+from dashboard.models import *
 
 # Create your views here.
 
 @login_required(login_url="login")
 @admins_only
 def contact_us(request):
+    # data nav bar
+    settings = Setting.objects.all()  # icon settings
+    profileImage = Profile.objects.filter(user=request.user)  # icon profile
     formContact = ContactForms()
     if request.method == "POST":
         formContact = ContactForms(request.POST)
@@ -17,4 +21,8 @@ def contact_us(request):
             obj.user = User.objects.get(id=request.user.id)
             obj.save()
             return redirect("dashboard")
+    context = {
+        'settings':settings,
+        'profileImage':profileImage,
+    }
     return render(request, "dashbord/pages/contact_us.html")
